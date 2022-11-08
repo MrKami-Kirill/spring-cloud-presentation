83c7bf47bccb9afe6bbbcc4b11066373a6a96305
kgaleev <kkiryukhin@rtech.ru>
2022-11-08 12:42:08 +0300

    initial commit

---
+++ proxy-module/src/main/java/spring/cloud/ProxyModuleApplication.java
@@ -0,0 +1,9 @@
+package spring.cloud;
+
+public class ProxyModuleApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ zipkin/readme.md
@@ -0,0 +1,5 @@
+Download from: https://zipkin.io/pages/quickstart
+
+To run: java -jar zipkin-server-2.12.9-exec.jar
+
+URL: http://localhost:9411
