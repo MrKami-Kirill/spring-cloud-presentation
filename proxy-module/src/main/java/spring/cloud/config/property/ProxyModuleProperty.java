6cd690aaf7a3c79cddf54bcfd8433a3ee8d0eb4f
kgaleev <kgaleev@rtech.ru>
2022-11-10 15:23:34 +0300

    spring cloud presentation v11

--- proxy-module/src/main/java/spring/cloud/config/property/ProxyModuleProperty.java
+++ proxy-module/src/main/java/spring/cloud/config/property/ProxyModuleProperty.java
@@ -1,11 +1,15 @@
 package spring.cloud.config.property;
 
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
+@Data
 @Component
-@Getter
+@ConfigurationProperties("api.path")
+@RefreshScope
 public class ProxyModuleProperty {
 
     @Value("${api.path.additional}")
@@ -14,5 +18,17 @@ public class ProxyModuleProperty {
     private String clientsPath;
     @Value("${api.path.client-by-id}")
     private String clientByIdPath;
+    @Value("${api.path.client-contacts}")
+    private String clientContactsPath;
+    @Value("${api.path.add-client}")
+    private String addClientPath;
+    @Value("${api.path.change-client}")
+    private String changeClientPath;
+    @Value("${api.path.add-contact-to-client}")
+    private String addContactToClientPath;
+    @Value("${api.path.delete-client}")
+    private String deleteClientPath;
+    @Value("${api.path.delete-client-contact}")
+    private String deleteClientContactPath;
 
 }
--- zipkin/readme.md
+++ zipkin/readme.md
@@ -1,5 +1,5 @@
 Download from: https://zipkin.io/pages/quickstart
 
-To run: java -jar zipkin-server-2.12.9-exec.jar
+To run: java -jar zipkin-server.jar
 
 URL: http://localhost:9411
